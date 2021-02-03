e8d06130667b39561ce8448898122dacc8497663
Justice <justice.douglas@hey.com>
2021-02-02 22:15:41 -0500

    Added login and logout feature to project

--- user_management_practice/users/urls.py
+++ user_management_practice/users/urls.py
@@ -1,6 +1,17 @@
-from django.conf.urls import url
+""" Commented out code fails to output expected results.
+Figure out why. """
+# from django.conf.urls import url, include
+# from users.views import dashboard
+
+# urlpatterns = [
+#     url(r"^accounts/", include("django.contrib.auth.urls")), # potential source of issue
+#     url(r"^dashboard/", dashboard, name="users/dashboard"),
+# ]
+
+from django.conf.urls import include, url
 from users.views import dashboard
 
 urlpatterns = [
-    url(r"^dashboard/", dashboard, name="users/dashboard"),
+    url(r"^accounts/", include("django.contrib.auth.urls")),
+    url(r"^dashboard/", dashboard, name="dashboard"),
 ]
